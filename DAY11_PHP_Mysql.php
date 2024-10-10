483a8241f01457991bbc61e911af3471caf2acf3
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2024-10-10 09:48:19 +0530

    PHP and Mysql connectivity

--- DAY11_PHP_Mysql.php
+++ DAY11_PHP_Mysql.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
-$dbname = "test"
+$password = "";
+$dbname = "joswin_db";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
